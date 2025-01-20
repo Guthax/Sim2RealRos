6c0f4f05e7bf920ca815cd5e692dde2990df1323
Guthax <jurrbuitenweg@gmail.com>
2025-01-20 12:43:05 +0100

    created ros node

--- packages/model2wheels/src/model2wheels.py
+++ packages/model2wheels/src/model2wheels.py
@@ -1,12 +1,37 @@
+import os
+import rospy
+from duckietown.dtros import DTROS, NodeType
+from duckietown_msgs.msg import WheelsCmdStamped
+
 class Model2WheelNode(DTROS):
     def __init__(self, node_name):
         # initialize the DTROS parent class
         super(Model2WheelNode, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
-        # static parameters
-        vehicle_name = os.environ['VEHICLE_NAME']
-        wheels_topic = f"/{vehicle_name}/wheels_driver_node/wheels_cmd"
-        # form the message
-        self._vel_left = THROTTLE_LEFT * DIRECTION_LEFT
-        self._vel_right = THROTTLE_RIGHT * DIRECTION_RIGHT
-        # construct publisher
-        self._publisher = rospy.Publisher(wheels_topic, WheelsCmdStamped, queue_size=1)
+	self._vehicle_name = os.environ['VEHICLE_NAME']
+        self._camera_topic = f"/{self._vehicle_name}/camera_node/image/compressed"
+	self._wheels_topic = f"/{self._vehicle_name}/wheels_driver_node/wheels_cmd"
+        # bridge between OpenCV and ROS
+        self._bridge = CvBridge()
+        # create window
+        self._window = "camera-reader"
+        cv2.namedWindow(self._window, cv2.WINDOW_AUTOSIZE)
+        # construct subscriber
+        self.camera_sub = rospy.Subscriber(self._camera_topic, CompressedImage, self.camera_callback)
+	self.wheel_pub = rospy.Publisher(self._wheels_topic, WheelsCmdStamped, queue_size=1)
+
+    def camera_callback(self, msg):
+        # convert JPEG bytes to CV image
+        image = self._bridge.compressed_imgmsg_to_cv2(msg)
+        # display frame
+        cv2.imshow(self._window, image)
+        cv2.waitKey(1)
+	self.publish_vels(0.2,0.2)
+
+
+    def publish_vels(vel_1: float, vel_2: float):
+	message = WheelsCmdStamped(vel_left=self.vel_1, vel_right=vel_2)
+	self.wheel_pub.publish(message)
+
+    def on_shutdown(self):
+        stop = WheelsCmdStamped(vel_left=0, vel_right=0)
+        self.wheel_pub.publish(stop)
